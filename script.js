f042ad49bda24e6771820fa884b88c4ee7a91001
Olamn-WebGIS <olamilekanfavour03@gmail.com>
2026-05-28 07:44:40 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -121,3 +121,18 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
+
+// Closes the drawer overlay dynamically when clicking any menu link
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    menuToggle.classList.remove('active');
+  });
+});
